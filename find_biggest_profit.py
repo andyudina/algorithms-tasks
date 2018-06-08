62f53848c0aa32ec56ec9e6876e950bce8b6e8d5
Anastasia <anastasia.diudina@gmail.com>
2018-06-08 19:53:07 +0300

    Gready solution for biggest profit problem

--- find_biggest_profit.py
+++ find_biggest_profit.py
@@ -12,19 +12,39 @@ def get_max_profit_simple(stock_prices):
     Prices are in order: minimum price should go before maximum price 
     in an array
 
-    Simple implementaton uses O^2 algorythm with double for loops
+    Simple implementaton uses O(n^2) algorythm with double for loops
     """
-    assert len(stock_prices) > 0, \
+    assert len(stock_prices) > 1, \
       'Stock prices list can not be empty'
     value_diff = stock_prices[len(stock_prices) - 1] - \
                  stock_prices[0]
-    for i in range(len(stock_prices) - 1):
-        for j in range(i + 1, len(stock_prices)):
+    for i in xrange(len(stock_prices) - 1):
+        for j in xrange(i + 1, len(stock_prices)):
             curr_value = stock_prices[j] - stock_prices[i]
             if curr_value > value_diff:
                 value_diff = curr_value
     return value_diff
 
+def get_max_profit_greedy(stock_prices):
+    """
+    Accepts list of stock prices.
+    Returns the biggest difference between two individual prices
+    Prices are in order: minimum price should go before maximum price 
+    in an array
+
+    Simple implementaton uses O(1) algorythm with one time going through array
+    """
+    assert len(stock_prices) > 1, \
+      'Stock prices list must have more than one element'
+
+    min_price  = stock_prices[0]
+    max_profit = stock_prices[1] - stock_prices[0]
+    for current_index in xrange(1, len(stock_prices)):
+        current_price = stock_prices[current_index]
+        current_profit = current_price - min_price
+        max_profit = max(max_profit, current_profit)
+        min_price  = min(min_price, current_price)
+    return max_profit
 
 class TestGetMaxProfitFunctionMixin(object):
     """
@@ -73,16 +93,6 @@ class TestGetMaxProfitFunctionMixin(object):
             self.get_max_profit(stock_prices),
             7)
 
-    def test_stock_list_with_one_element(self):
-        """
-        We return correct max profit value
-        when list has only one element
-        """
-        stock_prices = [10]
-        self.assertEqual(
-            self.get_max_profit(stock_prices),
-            0)
-
     def test_stock_list_with_equal_elements(self):
         """
         We return correct max profit value
@@ -92,6 +102,26 @@ class TestGetMaxProfitFunctionMixin(object):
         self.assertEqual(
             self.get_max_profit(stock_prices),
             0)
+
+    def test_stock_list__last_element_is_minimum(self):
+        """
+        We return correct max profit value
+        when list has global minimum in last place
+        """
+        stock_prices = [8, 9, 5, 6, 3]
+        self.assertEqual(
+            self.get_max_profit(stock_prices),
+            1)
+
+    def test_stock_list__minimum_element_after_best_pair(self):
+        """
+        We return correct max profit value
+        when list has global minimum element after best fait
+        """
+        stock_prices = [5, 11, 3, 4]
+        self.assertEqual(
+            self.get_max_profit(stock_prices),
+            6)
 
     def test_stock_list_with_zero_elements(self):
         """
@@ -105,12 +135,25 @@ class TestGetMaxProfitFunctionSimple(
         TestGetMaxProfitFunctionMixin,
         unittest.TestCase):
     """
-    Test simmple implementation of get_max_profi_function
+    Test simple implementation of get_max_profit_function_simpe
     with double loops
     """
 
     def get_max_profit(self, stock_prices):
         return get_max_profit_simple(stock_prices)
+
+
+class TestGetMaxProfitFunction(
+        TestGetMaxProfitFunctionMixin,
+        unittest.TestCase):
+    """
+    Test geedy implementation of get_max_profi_function
+    with double loops
+    """
+
+    def get_max_profit(self, stock_prices):
+        return get_max_profit_greedy(stock_prices)
+
 
 if __name__ == '__main__':
     unittest.main()
